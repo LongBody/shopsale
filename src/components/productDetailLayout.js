913e583c1c7b177e63b8d0b85df980f3f64572ac
LongBody <54003924+LongBody@users.noreply.github.com>
2020-08-29 22:26:01 +0700

    up

--- src/components/productDetailLayout.js
+++ src/components/productDetailLayout.js
@@ -39,7 +39,7 @@ const StyleStarNone = {
 
 function ProductDetail(props) {
     const history = useHistory();
-    let user = JSON.parse(localStorage.getItem("user"));
+    let user = JSON.parse(localStorage.getItem("userShopsale"));
     const [pro, setPro] = useState({
         title: "Loading...",
         price: "",
@@ -47,10 +47,11 @@ function ProductDetail(props) {
     })
 
 
+
     const [bioAsync, setBioAsync] = useState(false)
 
-    const fetchData = async() => {
-        const callApiData = await callApi("product/" + props.match.params.id).then(async(response) => {
+    const fetchData = async () => {
+        const callApiData = await callApi("product/" + props.match.params.id).then(async (response) => {
             let data = await response.data
             let dataConvert = {
                 id: data._id,
@@ -69,114 +70,81 @@ function ProductDetail(props) {
 
     useEffect(() => {
         fetchData()
-    }, []);
+    },[]);
 
 
     function newRow(text) {
         let result = text.split('\n')
         return result.map((item, key) => {
-            return <div key = { key } > -{ item } < /div>
+            return <div key={key}> - {item}</div>
         })
     }
 
-    return ( <
-        div >
-        <
-        Header / >
+    return (
+        <div>
+            <Header />
 
-        <
-        Container style = {
-            { paddingTop: 110 } } >
-        <
-        div style = {
-            { padding: 40, paddingTop: 50, backgroundColor: "#fff", height: "100%" } } > {
-            pro ?
-            <
-            Grid container spacing = { 2 }
-            xs = { 3 }
-            sm = { 8 }
-            md = { 12 }
-            lg = { 12 } >
-            <
-            Grid item xs = { 12 }
-            xs = { 10 }
-            sm = { 10 }
-            md = { 6 }
-            lg = { 5 } >
-            <
-            img src = { pro.imageUrl }
-            style = { styleImage }
-            /> <
-            div style = {
-                { marginTop: 50 } } >
-            <
-            h4 > CHI TIẾT SẢN PHẨM < /h4> {
-                bioAsync ?
-                    newRow(pro.bio) :
-                    "Loading"
+            <Container style={{ paddingTop: 110 }}>
+                <div style={{padding: 40,paddingTop:50, backgroundColor: "#fff",height:"100%" }}>
+                    {
+                        pro ?
+                            <Grid container spacing={2} xs={3} sm={8} md={12} lg={12}>
+                                <Grid item xs={12} xs={10} sm={10} md={6} lg={5}>
+                                    <img src={pro.imageUrl} style={styleImage} />
+                                    <div style={{marginTop:50}}>
+                                        <h4>CHI TIẾT SẢN PHẨM</h4>
+                                        {
+                                            bioAsync ?
+                                                newRow(pro.bio)
+                                                : "Loading"
 
-            } <
-            /div> <
-            /Grid> <
-            Grid item xs = { 12 }
-            xs = { 10 }
-            sm = { 10 }
-            md = { 6 }
-            lg = { 6 } >
-            <
-            h2 > { pro.title } < /h2> <
-            h3 > { showRating(pro.star) } < /h3> <
-            h1 style = { stylePrice } > ₫{ convertPrice(pro.price) } < /h1> <
-            h4 > Vận Chuyển :
-                <
-                span style = {
-                    { marginLeft: 10 } } >
-                <
-                img src = "https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/9d21899f3344277e34d40bfc08f60bc7.png"
-            style = {
-                { height: 20 } }
-            />
-            Miễn Phí Vận Chuyển <
-            /span> <
-            /h4> <
-            div >
-            <
-            Button variant = "outlined"
-            color = "primary"
-            onClick = {
-                () => {
-                    addToCart(pro, 1, true)
-                }
-            } >
-            Thêm Vào Giỏ Hàng <
-            /Button> <
-            Button variant = "contained"
-            color = "primary"
-            style = {
-                { marginLeft: 10 } }
-            onClick = {
-                () => {
-                    buyNow()
-                }
-            } >
-            Mua Ngay <
-            /Button> <
-            /div> <
-            /Grid> <
-            /Grid>: "Loading"
-        } <
-        /div>
-
-        <
-        /Container> <
-        /div >
+                                        }
+                                    </div>
+                                </Grid>
+                                <Grid item xs={12} xs={10} sm={10} md={6} lg={6}>
+                                    <h2>{pro.title}</h2>
+                                    <h3>{showRating(pro.star)}</h3>
+                                    <h1 style={stylePrice}>₫{convertPrice(pro.price)}</h1>
+                                    <h4>Vận Chuyển :
+                                        <span style={{ marginLeft: 10 }}>
+                                            <img src="https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/9d21899f3344277e34d40bfc08f60bc7.png"
+                                                style={{ height: 20 }}
+                                            />
+                                        Miễn Phí Vận Chuyển
+                                        </span>
+                                    </h4>
+                                    <div>
+                                        <Button variant="outlined" color="primary"
+                                            onClick={() => {
+                                                addToCart(pro, 1, true)
+                                            }}
+                                        >
+                                            Thêm Vào Giỏ Hàng
+                                         </Button>
+                                        <Button variant="contained" color="primary" style={{ marginLeft: 10 }}
+                                            onClick={() => {
+                                                buyNow()
+                                            }}
+                                        >
+                                            Mua Ngay
+                                        </Button>
+                                    </div>
+                                </Grid>
+                            </Grid>
+                            : "Loading"
+                    }
+                </div>
+               
+            </Container>
+        </div >
 
     );
 
     function addToCart(prop, quantity, checked) {
         if (user) {
             props.addToCart(prop, quantity, checked)
-        } else {
+        }
+        else {
             history.push("/sign-in");
         }
 
@@ -185,7 +153,8 @@ function ProductDetail(props) {
     function buyNow() {
         if (user) {
             swal("Thành Công", "Đã Mua Hàng", "success");
-        } else {
+        }
+        else {
             history.push("/sign-in");
         }
 
@@ -198,30 +167,32 @@ function showRating(rating) {
     let result = [];
 
     for (let index = 0; index < rating; index++) {
-        result.push( < i className = "fas fa-star"
-            style = { StyleStar }
-            key = { index } > < /i>)
+        result.push(<i className="fas fa-star" style={StyleStar} key={index}></i>)
+    }
+
+    for (let index2 = 0; index2 < (5 - rating); index2++) {
+        result.push(<i className="far fa-star" key={index2 + 100} style={StyleStarNone}></i>)
+    }
+    return result
+
+}
+
+
+
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        addToCart: (product, quantity, checked) => {
+            dispatch(actions.addToCart(product, quantity, checked))
         }
+    }
+}
 
-        for (let index2 = 0; index2 < (5 - rating); index2++) {
-            result.push( < i className = "far fa-star"
-                key = { index2 + 100 }
-                style = { StyleStarNone } > < /i>)
-            }
-            return result
 
-        }
+export default connect(null, mapDispatchToProps)(ProductDetail);
 
 
 
 
-        const mapDispatchToProps = (dispatch, ownProps) => {
-            return {
-                addToCart: (product, quantity, checked) => {
-                    dispatch(actions.addToCart(product, quantity, checked))
-                }
-            }
-        }
 
 
-        export default connect(null, mapDispatchToProps)(ProductDetail);
--- src/components/productFlashSaleDetailLayout .js
+++ src/components/productFlashSaleDetailLayout .js
@@ -66,8 +66,8 @@ function ProductDetail(props) {
 
     const [bioAsync, setBioAsync] = useState(false)
 
-    const fetchData = async() => {
-        const callApiData = await callApi("productFlashSale/" + props.match.params.id).then(async(response) => {
+    const fetchData = async () => {
+        const callApiData = await callApi("productFlashSale/" + props.match.params.id).then(async (response) => {
             let data = await response.data
             let dataConvert = {
                 id: data._id,
@@ -95,119 +95,84 @@ function ProductDetail(props) {
         let result = text.split('\n')
         console.log(text)
         return result.map((item, key) => {
-            return <div key = { key } > -{ item } < /div>
+            return <div key={key}> - {item}</div>
         })
     }
-
     function salePrice(price, sale) {
         let priceSale = price - price * (sale / 100)
         return convertPrice(priceSale)
     }
 
-    return ( <
-        div >
-        <
-        Header / >
+    return (
+        <div>
+            <Header />
 
-        <
-        Container style = {
-            { paddingTop: 110 } } >
-        <
-        div style = {
-            { padding: 20, backgroundColor: "#fff" } } > {
-            pro ?
-            <
-            Grid container spacing = { 2 }
-            xs = { 3 }
-            sm = { 8 }
-            md = { 12 }
-            lg = { 12 } >
-            <
-            Grid item xs = { 12 }
-            xs = { 10 }
-            sm = { 10 }
-            md = { 6 }
-            lg = { 4 } >
-            <
-            img src = { pro.imageUrl }
-            style = { styleImage }
-            /> <
-            div style = {
-                { marginTop: 50 } } >
-            <
-            h4 > CHI TIẾT SẢN PHẨM < /h4> {
-                bioAsync ?
-                    newRow(pro.bio) :
-                    "Loading"
+            <Container style={{ paddingTop: 110 }}>
+                <div style={{ padding: 20, backgroundColor: "#fff" }}>
+                    {
+                        pro ?
+                            <Grid container spacing={2} xs={3} sm={8} md={12} lg={12}>
+                                <Grid item xs={12} xs={10} sm={10} md={6} lg={4}>
+                                    <img src={pro.imageUrl} style={styleImage} />
+                                    <div style={{ marginTop: 50 }}>
+                                        <h4>CHI TIẾT SẢN PHẨM</h4>
+                                        {
+                                            bioAsync ?
+                                                newRow(pro.bio)
+                                                : "Loading"
 
-            } <
-            /div> <
-            /Grid> <
-            Grid item xs = { 12 }
-            xs = { 10 }
-            sm = { 10 }
-            md = { 6 }
-            lg = { 6 } >
-            <
-            h2 > { pro.title } < /h2> <
-            h3 > { showRating(pro.star) } < /h3> <
-            h1 style = { stylePrice } > ₫{ pro.price ? salePrice(pro.price, pro.sale) : "" } <
-            span style = { styleSale } > Giảm { pro.sale } % < /span> <
-            /h1> <
-            h3 style = { stylePriceSale } > ₫{ convertPrice(pro.price) }
+                                        }
+                                    </div>
+                                </Grid>
+                                <Grid item xs={12} xs={10} sm={10} md={6} lg={6}>
+                                    <h2>{pro.title}</h2>
+                                    <h3>{showRating(pro.star)}</h3>
+                                    <h1 style={stylePrice}>₫{pro.price ? salePrice(pro.price, pro.sale) : ""}
+                                        <span style={styleSale}>Giảm {pro.sale}%</span>
+                                    </h1>
+                                    <h3 style={stylePriceSale}>₫{convertPrice(pro.price)}
 
-            <
-            /h3>
+                                    </h3>
 
-            <
-            h4 > Vận Chuyển :
-                <
-                span style = {
-                    { marginLeft: 10 } } >
-                <
-                img src = "https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/9d21899f3344277e34d40bfc08f60bc7.png"
-            style = {
-                { height: 20 } }
-            />
-            Miễn Phí Vận Chuyển <
-            /span> <
-            /h4> <
-            div >
-            <
-            Button variant = "outlined"
-            color = "primary"
-            onClick = {
-                () => {
-                    addToCart(pro, 1, true)
-                }
-            } >
-            Thêm Vào Giỏ Hàng <
-            /Button> <
-            Button variant = "contained"
-            color = "primary"
-            style = {
-                { marginLeft: 10 } }
-            onClick = {
-                () => {
-                    buyNow()
-                }
-            } >
-            Mua Ngay <
-            /Button> <
-            /div> <
-            /Grid> <
-            /Grid>: "Loading"
-        } <
-        /div> <
-        /Container> <
-        /div>
+                                    <h4>Vận Chuyển :
+                                        <span style={{ marginLeft: 10 }}>
+                                            <img src="https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/9d21899f3344277e34d40bfc08f60bc7.png"
+                                                style={{ height: 20 }}
+                                            />
+                                        Miễn Phí Vận Chuyển
+                                        </span>
+                                    </h4>
+                                    <div>
+                                        <Button variant="outlined" color="primary"
+                                            onClick={() => {
+                                                addToCart(pro, 1, true)
+                                            }}
+                                        >
+                                            Thêm Vào Giỏ Hàng
+                                         </Button>
+                                        <Button variant="contained" color="primary" style={{ marginLeft: 10 }}
+                                            onClick={() => {
+                                                buyNow()
+                                            }}
+                                        >
+                                            Mua Ngay
+                                        </Button>
+                                    </div>
+                                </Grid>
+                            </Grid>
+                            : "Loading"
+                    }
+                </div>
+            </Container>
+        </div>
 
     );
 
     function addToCart(prop, quantity, checked) {
         if (user) {
             props.addToCart(prop, quantity, checked)
-        } else {
+        }
+        else {
             history.push("/sign-in");
         }
 
@@ -216,7 +181,8 @@ function ProductDetail(props) {
     function buyNow() {
         if (user) {
             swal("Thành Công", "Đã Mua Hàng", "success");
-        } else {
+        }
+        else {
             history.push("/sign-in");
         }
 
@@ -229,30 +195,32 @@ function showRating(rating) {
     let result = [];
 
     for (let index = 0; index < rating; index++) {
-        result.push( < i className = "fas fa-star"
-            style = { StyleStar }
-            key = { index } > < /i>)
+        result.push(<i className="fas fa-star" style={StyleStar} key={index}></i>)
+    }
+
+    for (let index2 = 0; index2 < (5 - rating); index2++) {
+        result.push(<i className="far fa-star" key={index2 + 100} style={StyleStarNone}></i>)
+    }
+    return result
+
+}
+
+
+
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        addToCart: (product, quantity, checked) => {
+            dispatch(actions.addToCart(product, quantity, checked))
         }
+    }
+}
 
-        for (let index2 = 0; index2 < (5 - rating); index2++) {
-            result.push( < i className = "far fa-star"
-                key = { index2 + 100 }
-                style = { StyleStarNone } > < /i>)
-            }
-            return result
 
-        }
+export default connect(null, mapDispatchToProps)(ProductDetail);
 
 
 
 
-        const mapDispatchToProps = (dispatch, ownProps) => {
-            return {
-                addToCart: (product, quantity, checked) => {
-                    dispatch(actions.addToCart(product, quantity, checked))
-                }
-            }
-        }
 
 
-        export default connect(null, mapDispatchToProps)(ProductDetail);
