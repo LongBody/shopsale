80042c2e6cfe8743ee5f8df68f5bdfd666bf365a
LongBody <54003924+LongBody@users.noreply.github.com>
2020-09-06 16:01:57 +0700

    up

--- src/components/googleSigin.js
+++ src/components/googleSigin.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
 import { useHistory } from "react-router-dom";
+import Button from '@material-ui/core/Button';
 export default function GoogleSignIn() {
   let history = useHistory();
 
@@ -19,7 +20,13 @@ export default function GoogleSignIn() {
     <div style={{display:"flex" , justifyContent:"center" , paddingBottom:10 , width:"100%" , backgroundColor:"rgb(255, 255, 255)"}}>
       <GoogleLogin
         clientId="1079345342714-8q3900edhd8glu594i1kbgovile1bgio.apps.googleusercontent.com"
-        buttonText="Sign in with Google"
+        render={renderProps => (
+          <Button variant="contained" onClick={renderProps.onClick} disabled={renderProps.disabled} 
+           className="btn-sign-in-gg"
+          >
+            <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg " style={{height:20 , marginRight:10}}/>
+             Sign in with Google</Button>
+        )}
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
         cookiePolicy={'single_host_origin'}
