38ab644a38c9f316a46ab5545c8d72b12d17eaa3
LongBody <54003924+LongBody@users.noreply.github.com>
2020-09-07 16:56:40 +0700

    up

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -19,7 +19,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="/shopsale">
+      <Link color="inherit" href="/">
         ShopSale
       </Link>{" "}
       {new Date().getFullYear()}
