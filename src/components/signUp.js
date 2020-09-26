48a12f0d041e398aafbb94230187382ec9eb25e0
LongBody <54003924+LongBody@users.noreply.github.com>
2020-09-26 14:38:14 +0700

    update

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -223,7 +223,7 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="/#/shopsale/sign-in" variant="body2">
+              <Link href="/sign-in" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
